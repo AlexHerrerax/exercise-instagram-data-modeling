4e8aa710d1343e8ffdc42a4f7bce22b01e8e661e
Alex Herrera <axherrerax@gmail.com>
2021-04-27 20:07:48 +0000

    Instagram

--- src/models.py
+++ src/models.py
@@ -8,23 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email=Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    name_user=Column(String(250), nullable=False)
+    image=Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'Post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id =Column(Integer, ForeignKey(User.id) )
+    comments = Column(String(250), nullable=False)
+    like= Column(Integer, nullable=False)
+    post = Column(String(250), nullable=False)
+    creation_date=Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ ='Comment'
+    id = Column(Integer, primary_key=True)
+    user_id =Column(Integer, ForeignKey(User.id) )
+    post_id=Column(Integer, ForeignKey(Post.id) )
+    comment =Column(String(250), nullable=False)
+    creation_date =Column(String(250), nullable=False)
+
+class Save(Base):
+    __tablename__ ='Save'
+    id = Column(Integer, primary_key=True)
+    user_id =Column(Integer, ForeignKey(User.id) )
+    post_id=Column(Integer, ForeignKey(Post.id) )
+    save_date =Column(String(250), nullable=False)
+
+
+
+    
 
     def to_dict(self):
         return {}
